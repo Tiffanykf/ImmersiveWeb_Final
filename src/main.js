4fc4b592bf163eb1b2b0025965770b5d99dbf3a1
Tiffanykf <52668951+Tiffanykf@users.noreply.github.com>
2025-03-16 21:09:27 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -95,10 +95,7 @@ function init() {
 
 	window.addEventListener('click', () => {
 		//remove loading screen 
-		loadingScreen.style.opacity = '0';
-		setTimeout(() => {
-			loadingScreen.style.display = 'none'; 
-		}, 50);
+		loadingScreen.style.display = 'none'
 		//Check for click on sphere
 		checkSphereClick()
 		//play audio
